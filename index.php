4a32ef714820f3f2ce3bbd81210e483c69c68921
CreativeDream <yuliangagarin@mail.ru>
2014-01-09 19:59:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>PHP-VideoGet 1.0</title>
+  <title>Get Video information by URL</title>
 </head>
 
 <body>
@@ -11,12 +11,12 @@
     include("videoget.Class.php");
 	
     $url = "http://www.youtube.com/watch?v=T6DJcgm3wNY";
-    #$url = '<iframe width="560" height="315" src="http://www.youtube.com/embed/T6DJcgm3wNY?rel=0" frameborder="0" allowfullscreen></iframe>';
-
-    $video = new videoGet($url);
+	#$url = '<iframe width="560" height="315" src="http://www.youtube.com/embed/T6DJcgm3wNY?rel=0" frameborder="0" allowfullscreen></iframe>';
+    $get = array('title','description','image','video','url','site_name');
 	
-	$video->get = array('title','description','image','video','url','site_name');
-    $video = $video->getVideoData();
+    $video = new videoGet($url, $get);
+	
+	$video = $video->getVideoData();
 	
 	if($video){
 		echo "<pre>";
